411d0895295d97739a1928cd905ec204c4c375bd
kenkenpa198 <kenkenpa198@gmail.com>
2021-02-06 01:50:20 +0900

    [update][add]いろいろ拡張

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,28 +1,42 @@
-##### Commands #####
+##### zsh Arias #####
 # Linux Command
+alias src="source ~/.zshrc"
 alias la="ls -la"
 alias ll="ls -la"
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias h="history"
+alias hg="history | grep"
+
 
 # Docker
 alias d="docker"
 alias dc="docker-compose"
 
 # Git
+alias g="git"
+alias gs="git status"
 alias gb="git branch"
 alias gc="git checkout"
-alias gm="git merge"
-alias gs="git status"
-alias gss="git status -s"
+alias gct="git commit"
+alias gctm="git commit -m"
+alias gg="git grep"
 alias ga="git add"
 alias gaa="git add -A"
-alias gcm="git commit -m"
+alias gd="git diff"
+alias gm="git merge"
+alias gss="git status -s"
 alias gp="git push"
 alias gpo="git push origin"
 alias gpom="git push origin main"
+alias gpod="git push origin develop"
 alias gf="git fetch"
 alias gpl="git pull"
 alias gl="git log"
 alias gll="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gst="git stash"
+alias gsts="git stash save"
+alias gstp="git stash pop"
 
 
 ##### Homebrew #####
