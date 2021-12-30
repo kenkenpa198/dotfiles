2ea4cd79e6bae55366846bbd49bd0e42ec943584
kenkenpa198 <kenkenpa198@gmail.com>
2021-12-30 22:17:33 +0900

    [update ]winget用ファイルなど追加更新。

---
+++ .bin/winget.bat
@@ -0,0 +1,41 @@
+:: 管理者として実行してね
+:: 先に Microsoft Store へログインしておいてね
+:: ほんで「アプリインストーラー」をダウンロードしておいてね
+
+:: winget からインストール
+winget install Microsoft.VisualStudioCode
+winget install Microsoft.WindowsTerminal
+winget install --force Google.Chrome
+winget install Lexikos.AutoHotkey
+winget install QL-Win.QuickLook
+winget install Discord.Discord
+winget install OBSProject.OBSStudio
+winget install Valve.Steam
+winget install WinMerge.WinMerge
+winget install Apple.iTunes
+winget install 7zip.7zip
+winget install Docker.DockerDesktop
+winget install Atlassian.Sourcetree
+winget install Git.Git
+winget install -e --id BlenderFoundation.Blender
+winget install Logitech.Options
+
+:: Microsoft Store からインストール
+winget install iCloud
+winget install Python 3.10
+
+:: WSL環境のインストール
+wsl --install -d Ubuntu
+
+:: 下記は手動インストール！！
+:: alt-ime-ahk / https://github.com/karakaram/alt-ime-ahk/releases
+:: Change Key / https://forest.watch.impress.co.jp/library/software/changekey/
+:: dotfiles / https://github.com/kenkenpa198/dotfiles
+:: サクラエディタ / https://sakura-editor.github.io/
+:: RictyDiminished-for-Powerline / https://github.com/mzyy94/RictyDiminished-for-Powerline
+:: Noto Sans Japanese / https://fonts.google.com/noto/specimen/Noto+Sans+JP
+:: Clip Studio Paint / https://www.clipstudio.net/ja/dl/
+:: Wacom driver / https://tablet.wacom.co.jp/download/
+:: Vroid Studio / https://vroid.com/
+:: 3tene / https://3tene.com/
+:: Valorant / https://playvalorant.com/ja-jp/download/
--- .config/AutoHotKey/KemCmds.ahk
+++ .config/AutoHotKey/KemCmds.ahk
@@ -1,7 +1,13 @@
-; 先に ChgKey を使って caps lock に F13 キーを登録しておく
-; F13 キーのスキャンコード：0x0064
+;;;;; kenkenpa198 コマンド集 ;;;;;
 
-;;;;; Emacs Commands light ;;;;;
+/*
+Emacs コマンドなど自分用に作成した ahk ファイル。
+
+先に ChgKey を使って Caps Lock に F13 キーを登録しておく。
+F13 キーのスキャンコード: 0x0064
+*/
+
+;;; Emacs Commands ;;;
 F13 & f::Right     ; forward char
 F13 & b::Left      ; backward char
 F13 & p::Up        ; previous line
@@ -11,17 +17,17 @@ F13 & e::END       ; move end of line
 F13 & d::Del       ; delete char
 F13 & h::BS        ; delete backward char
 F13 & m::Enter     ; newline
-
-F13 & k::
+F13 & k::          ; 切り取り
     send, +{END}
     send, ^x
 return
-F13 & y::^v
+F13 & y::^v        ; 貼り付け
 
-;;;;; Support Commands ;;;;;
-F13 & i::F7        ; カタカナ変換
+;;; 変換関連 ;;;
 F13 & u::F6        ; ひらがな変換
+F13 & i::F7        ; カタカナ変換
 F13 & o::F10       ; 半角英数字変換
 
+;;; その他 ;;;
 F13 & Space::#s    ; Windows 検索
 !q::!F4            ; アプリの終了
