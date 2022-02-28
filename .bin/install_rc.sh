7f7d0bca6d09907928fb6c75a1eac617dc3de46c
kenkenpa198 <kenkenpa198@gmail.com>
2022-03-01 02:44:32 +0900

    [move]ディレクトリ移動

--- .bin/install_rc.sh
+++
@@ -1,9 +0,0 @@
-##### install dotfiles to VSCode Remote Containers #####
-echo "===== dotfiles の読込を開始します ====="
-
-#!/bin/zsh
-ln -fs ~/dotfiles/.zshrc ~/
-
-source .zshrc
-
-echo "===== dotfiles の読込を完了しました ====="
