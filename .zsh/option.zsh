df117bb0fc358b1b50ed57a9de270b43926c649b
kenkenpa198 <kenkenpa198@gmail.com>
2022-03-04 15:52:36 +0900

    [update]history 関連の設定を追加

--- .zsh/option.zsh
+++ .zsh/option.zsh
@@ -1,4 +1,5 @@
 ##### Options #####
+
 # ビープ音停止
 setopt no_beep
 
@@ -13,3 +14,24 @@ autoload -U zmv
 
 # 移動したディレクトリを記録しておく。"cd -[Tab]"で移動履歴を一覧表示
 setopt auto_pushd
+
+# 履歴の保存先ファイル指定
+HISTFILE=~/.zsh_history
+
+# メモリに保存される履歴の件数を設定
+export HISTSIZE=1000
+
+# 履歴ファイルに保存される履歴の件数
+export SAVEHIST=1000
+
+# 同時に起動しているzshの間で history を共有する
+setopt share_history
+
+# history に保存するときに余分なスペースを削除する
+setopt hist_reduce_blanks
+
+# 同じコマンドを history に残さない
+setopt hist_ignore_all_dups
+
+# スペースから始まるコマンドを history に残さない
+setopt hist_ignore_space
