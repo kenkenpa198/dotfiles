899ed53e82efb01b8d00757f26c57157caf73f6d
kenkenpa198 <kenkenpa198@gmail.com>
2022-03-13 17:53:06 +0900

    [update][remove]いろいろ整理。今はもう使ってないアプリや環境変数を削除

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -5,13 +5,14 @@
 # 環境ごとに読み込む PATH
 case ${OSTYPE} in
 
-    # MacOS 向け設定
+    ### MacOS 向け設定 ###
     darwin*)
         # bin_M1
         export PATH="$PATH:/opt/homebrew/bin:/opt/homebrew/sbin"
 
         # bin
         export PATH="$PATH:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
+
         # pyenv
         export PATH="$PATH:$HOME/.pyenv/shims"
 
@@ -20,16 +21,10 @@ case ${OSTYPE} in
 
     ;;
 
-    # Linux 向け設定
+    ### Linux 向け設定 ###
     linux*)
-        # Linuxbrew
-        export PATH="$PATH:/home/linuxbrew/.linuxbrew/bin"
 
-        # Linuxbrew の実行パス
-        # pyenv の設定ファイルと競合して警告が出るのでエイリアスで指定。
-        alias brew='PATH=/home/linuxbrew/.linuxbrew/bin:/bin:/home/linuxbrew/.linuxbrew/sbin:/sbin brew'
-
-        # WSL 向け設定
+        ### WSL 向け設定 ###
         if uname -r | grep -i 'microsoft' > /dev/null ; then
             # Win 環境の環境変数を WSL 環境へ複製
             export PATH="$PATH:$Path"
