0253b5c75f0315f6d29a816ad5f3d4282f6d419b
kenkenpa198 <kenkenpa198@gmail.com>
2022-04-06 22:30:16 +0900

    [update]winget.bat へ DevToys 等を追加

--- .setup/bat/winget.bat
+++ .setup/bat/winget.bat
@@ -4,7 +4,8 @@
 :: 使用許諾契約に同意するオプションを環境変数へ登録
 set y="--accept-package-agreements"
 
-:: winget からインストール
+
+::::: winget からインストール :::::
 winget install Microsoft.VisualStudioCode %y%
 winget install Microsoft.AzureDataStudio %y%
 winget install --force Google.Chrome %y%
@@ -24,20 +25,33 @@ winget install Git.Git %y%
 winget install -e --id BlenderFoundation.Blender %y%
 winget install Logitech.Options %y%
 winget install xanderfrangos.twinkletray %y%
+winget install File-New-Project.EarTrumpet %y%
 
-:: Microsoft Store からインストール
+
+::::: Microsoft Store からインストール :::::
+winget install devtoys %y%
 winget install iCloud %y%
 winget install "Python 3.10" %y%
 
-:: 下記は手動インストール！！
-:: alt-ime-ahk / https://github.com/karakaram/alt-ime-ahk/releases
-:: Change Key / https://forest.watch.impress.co.jp/library/software/changekey/
+:: Compress
+winget install 9N1FHQZ38LHT %y%
+
+
+::::: 手動インストール :::::
+:: TODO: そのうちこの辺も自動化したい
+
 :: dotfiles / https://github.com/kenkenpa198/dotfiles
+
+:: Change Key / https://forest.watch.impress.co.jp/library/software/changekey/
+:: alt-ime-ahk / https://github.com/karakaram/alt-ime-ahk/releases
 :: サクラエディタ / https://sakura-editor.github.io/
-:: RictyDiminished-for-Powerline / https://github.com/mzyy94/RictyDiminished-for-Powerline
-:: Noto Sans Japanese / https://fonts.google.com/noto/specimen/Noto+Sans+JP
 :: Clip Studio Paint / https://www.clipstudio.net/ja/dl/
 :: Wacom driver / https://tablet.wacom.co.jp/download/
 :: Vroid Studio / https://vroid.com/
 :: 3tene / https://3tene.com/
+
+:: RictyDiminished-for-Powerline / https://github.com/mzyy94/RictyDiminished-for-Powerline
+:: HackGen / https://github.com/yuru7/HackGen
+:: Noto Sans Japanese / https://fonts.google.com/noto/specimen/Noto+Sans+JP
+
 :: Valorant / https://playvalorant.com/ja-jp/download/
