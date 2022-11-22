2ba3ec878341e9fcd6b5416b14a349f07b11f879
kenkenpa198 <kenkenpa198@gmail.com>
2022-11-22 19:07:36 +0900

    [update]theme.zsh をコンパクトな表示に改修

--- .zsh/rc/theme.zsh
+++ .zsh/rc/theme.zsh
@@ -1,48 +1,6 @@
-# 参考: https://tomiylab.com/2020/03/prompt/
-
-##### プロンプトの設定 #####
-export CLICOLOR=1
-
-echo Hello!
-autoload -Uz compinit && compinit # Gitの補完を有効化
-
-function left-prompt {
-    name_t='179m%}'            # user name text clolr
-    name_b='000m%}'            # user name background color
-    path_t='255m%}'            # path text clolr
-    path_b='031m%}'            # path background color
-    arrow='087m%}'             # arrow color
-    text_color='%{\e[38;5;'    # set text color
-    back_color='%{\e[30;48;5;' # set background color
-    reset='%{\e[0m%}'          # reset
-    sharp='\uE0B0'             # triangle
-
-    # 実行環境によってパスの背景色を変更
-    DistributorId=`lsb_release --id`
-    case ${DistributorId} in
-
-        # Ubuntu
-        # *Ubuntu)
-        #     path_b='202m%}'
-        # ;;
-
-        # Raspberry Pi OS
-        *Raspbian)
-            path_b='203m%}'
-        ;;
-    esac
-
-    user="${back_color}${name_b}${text_color}${name_t}"
-    dir="${back_color}${path_b}${text_color}${path_t}"
-
-    # HackGenNerd Console などパワーライン記号が半角の場合はこちら
-    echo "${user}%n@%m${back_color}${path_b}${text_color}${name_b}${sharp} ${dir}%~${reset}${text_color}${path_b}${sharp}${reset}\n${text_color}${arrow}$ ${reset}"
-
-    # Ricty Diminished for Powerline などパワーライン記号が全角の場合はこちら
-    # echo "${user}%n@%m${back_color}${path_b}${text_color}${name_b}${sharp}  ${dir}%~${reset}${text_color}${path_b}${sharp} ${reset}\n${text_color}${arrow}$ ${reset}"
-}
-
-PROMPT=`left-prompt`
+# 参考
+# https://qiita.com/koki0527/items/ca734df6fa86a8dbd241
+# https://tomiylab.com/2020/03/prompt/
 
 
 ##### コマンドの実行ごとに改行 #####
@@ -58,10 +16,9 @@ function precmd() {
 
 
 ##### git ブランチ名を色付きで表示させるメソッド #####
-function rprompt-git-current-branch {
+function prompt-git-current-branch {
     local branch_name st branch_status
 
-    branch='\ue0a0'
     color='%{\e[38;5;' # 文字色を設定
     green='114m%}'
     red='001m%}'
@@ -79,53 +36,37 @@ function rprompt-git-current-branch {
     # HackGenNerd Console などパワーライン記号が半角の場合はこちら
     if [[ -n `echo "$st" | grep "^nothing to"` ]]; then
         # 全て commit されてクリーンな状態
-        branch_status="${color}${green}${branch}"
+        branch_status="${color}${green}"
     elif [[ -n `echo "$st" | grep "^Untracked files"` ]]; then
         # git 管理されていないファイルがある状態
-        branch_status="${color}${red}${branch}?"
+        branch_status="${color}${red}?"
     elif [[ -n `echo "$st" | grep "^Changes not staged for commit"` ]]; then
         # git add されていないファイルがある状態
-        branch_status="${color}${red}${branch}+"
+        branch_status="${color}${red}+"
     elif [[ -n `echo "$st" | grep "^Changes to be committed"` ]]; then
         # git commit されていないファイルがある状態
-        branch_status="${color}${yellow}${branch}!"
+        branch_status="${color}${yellow}!"
     elif [[ -n `echo "$st" | grep "^rebase in progress"` ]]; then
         # コンフリクトが起こった状態
-        echo "${color}${red}${branch}!(no branch)${reset}"
+        echo "${color}${red}!(no branch)${reset}"
         return
     else
         # 上記以外の状態の場合
-        branch_status="${color}${blue}${branch}"
+        branch_status="${color}${blue}"
     fi
-
-    # Ricty Diminished for Powerline など記号が全角の場合はこちら
-    # if [[ -n `echo "$st" | grep "^nothing to"` ]]; then
-    #     # 全て commit されてクリーンな状態
-    #     branch_status="${color}${green}${branch} "
-    # elif [[ -n `echo "$st" | grep "^Untracked files"` ]]; then
-    #     # git 管理されていないファイルがある状態
-    #     branch_status="${color}${red}${branch} ?"
-    # elif [[ -n `echo "$st" | grep "^Changes not staged for commit"` ]]; then
-    #     # git add されていないファイルがある状態
-    #     branch_status="${color}${red}${branch} +"
-    # elif [[ -n `echo "$st" | grep "^Changes to be committed"` ]]; then
-    #     # git commit されていないファイルがある状態
-    #     branch_status="${color}${yellow}${branch} !"
-    # elif [[ -n `echo "$st" | grep "^rebase in progress"` ]]; then
-    #     # コンフリクトが起こった状態
-    #     echo "${color}${red}${branch} !(no branch)${reset}"
-    #     return
-    # else
-    #     # 上記以外の状態の場合
-    #     branch_status="${color}${blue}${branch} "
-    # fi
 
     # ブランチ名を色付きで表示する
     echo "${branch_status}$branch_name${reset}"
 }
 
-# プロンプトが表示されるたびにプロンプト文字列を評価、置換する
+
+##### プロンプト表示を実行 #####
+export CLICOLOR=1
+echo Hello!
+
+# プロンプトが表示されるたびにプロンプト文字列を評価し置換する
 setopt prompt_subst
 
-# プロンプトの右側にメソッドの結果を表示させる
-RPROMPT='`rprompt-git-current-branch`'
+# プロンプトを表示
+PROMPT='%F{179m%}%n@%m%F %F{white}[%B%~%b]%F `prompt-git-current-branch`
+%F{087m%}$%F{white} '
