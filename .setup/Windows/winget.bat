68a05ab45cc048b85927b58289cc5be843462176
kenkenpa198 <kenkenpa198@gmail.com>
2022-12-15 00:55:16 +0900

    [update]winget のソースを Microsoft Store 側からなるべくインストールするように変更

--- .setup/Windows/winget.bat
+++ .setup/Windows/winget.bat
@@ -5,50 +5,83 @@
 set y="--accept-package-agreements"
 
 
-::::: winget からインストール :::::
-winget install 7zip.7zip %y%
-winget install Avidemux.Avidemux %y%
-winget install -e --id BlenderFoundation.Blender %y%
-winget install Lexikos.AutoHotkey %y%
-winget install File-New-Project.EarTrumpet %y%
-winget install Git.Git %y%
-winget install Google.Chrome --force %y%
-winget install Apple.iTunes %y%
-winget install Discord.Discord %y%
-winget install Docker.DockerDesktop %y%
-winget install Logitech.Options %y%
-winget install LINE.LINE %y%
-winget install Microsoft.VisualStudioCode %y%
-winget install Microsoft.AzureDataStudio %y%
-winget install Mintty.WSLtty %y%
-winget install OBSProject.OBSStudio %y%
-winget install QL-Win.QuickLook %y%
-winget install Atlassian.Sourcetree %y%
-winget install xanderfrangos.twinkletray %y%
-winget install Valve.Steam %y%
-winget install WinMerge.WinMerge %y%
-winget install WinSCP.WinSCP %y%
-
-
-
 ::::: Microsoft Store からインストール :::::
-winget install devtoys %y%
-winget install iCloud %y%
-winget install "Python 3.10" %y%
+:: 7-Zip
+winget install XPDNKVCX4QD2DC %y%
+
+:: Blender
+winget install 9PP3C07GTVRH %y%
+
+:: Brave
+winget install XP8C9QZMS2PC1T %y%
 
 :: Compress
 winget install 9N1FHQZ38LHT %y%
 
+:: CLIP STUDIO PAINT
+winget install XPDP32QDDFL4PK %y%
+
+:: DevToys
+winget install 9PGCV4V3BK4W %y%
+
+:: Discord
+winget install XPDC2RH70K22MN %y%
+
+:: EarTrumpet
+winget install 9NBLGGH516XP %y%
+
+:: HyperX NGENUITY
+winget install 9P1TBXR6QDCX %y%
+
+:: iCloud
+winget install 9PKTQ5699M62 %y%
+
+:: iTunes
+winget install 9PB2MZ1ZMB1S %y%
+
+:: LINE
+winget install 9WZDNCRFJ2G6 %y%
+
+:: OBS Studio
+winget install XPFFH613W8V6LV %y%
+
+:: PowerToys
+winget install XP89DCGQ3K6VLD %y%
+
+:: Python 3.10
+winget install 9PJPW5LDXLZ5 %y%
+
+:: QuickLook
+winget install 9NV4BS3L1H4S %y%
+
+:: Twinkle Tray
+winget install 9PLJWWSV01LK %y%
+
+:: Visual Studio Code
+winget install XP9KHM4BK9FZ7Q %y%
+
+
+::::: winget からインストール :::::
+winget install Elgato.4KCaptureUtility %y%
+winget install Lexikos.AutoHotkey %y%
+winget install Avidemux.Avidemux %y%
+winget install Google.Chrome --force %y%
+winget install CaesiumImageCompressor %y%
+winget install Docker.DockerDesktop %y%
+winget install Git.Git %y%
+winget install Microsoft.AzureDataStudio %y%
+winget install RealVNC.VNCViewer %y%
+winget install WinMerge.WinMerge %y%
+winget install WinSCP.WinSCP %y%
+winget install Valve.Steam %y%
+
 
 ::::: 手動インストール :::::
-:: TODO: そのうちこの辺も自動化したい
-
-:: dotfiles / https://github.com/kenkenpa198/dotfiles
-
 :: Change Key / https://forest.watch.impress.co.jp/library/software/changekey/
 :: alt-ime-ahk / https://github.com/karakaram/alt-ime-ahk/releases
 :: サクラエディタ / https://sakura-editor.github.io/
-:: Clip Studio Paint / https://www.clipstudio.net/ja/dl/
+:: Free Video Compressor / https://www.gigafree.net/media/me/FreeVideoCompressor.html
+
 :: Wacom driver / https://tablet.wacom.co.jp/download/
 :: Vroid Studio / https://vroid.com/
 :: 3tene / https://3tene.com/
