266d8f101b3981d42711cb1c0e934ab0749fdfc8
kenkenpa198 <kenkenpa198@gmail.com>
2023-01-05 01:59:22 +0900

    [update]コメントの書式を統一

--- .zsh/rc/function/cj.zsh
+++ .zsh/rc/function/cj.zsh
@@ -1,7 +1,7 @@
 ##### CST to JST #####
 # CST（日本標準時）と JST（米国中部標準時）を出力・計算するシェル関数
 
-# 参考 :
+# 参考:
 # [date コマンド | コマンドの使い方(Linux) | hydroculのメモ](https://hydrocul.github.io/wiki/commands/date.html)
 # [日付を取得する | UNIX & Linux コマンド・シェルスクリプト リファレンス](https://shellscript.sunone.me/date.html)
 
