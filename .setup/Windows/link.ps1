bc2f17eb2684f70f1201eccb96db34f0d958f407
kenkenpa198 <kenkenpa198@gmail.com>
2023-01-08 21:17:22 +0900

    [update]Win 環境から WSL 環境へリンクを貼るコマンドを改善

--- .setup/Windows/link.ps1
+++ .setup/Windows/link.ps1
@@ -4,48 +4,49 @@
 # シンボリックリンク作成用 PowerShell スクリプト。
 # クラウド環境や WSL 環境のディレクトリ・設定ファイルを Win 環境から利用できるようにする。
 # WSL2 のディレクトリ上にある bat ファイルを Win 側から実行する場合、このシンボリックリンク設定を行った上で Win 側からその bat ファイルを実行したりショートカットを作成する。
+
+# New-Item -Value <<リンク先のパス>> -Path <<リンクを置くディレクトリ>> -Name <<リンクのファイル名>> -ItemType SymbolicLink
+
 # 参考:
 # [PowerShellで苦労して作るシンボリックリンク - Qiita](https://qiita.com/tokky_se/items/128a04e861b2863e2c99)
 # [Googleドライブのパスを英語に変更したい - 日日是好日](https://siromiuo.hateblo.jp/entry/2021/11/03/133144)
 
 
-##### ディレクトリのリンク設定 #####
-# [Win] ~\Documents\Files ---> [Google Drive] ~/Documents/Files
+##### Win 環境からクラウドストレージへのリンク #####
+# [Win] ~\Documents\Files ---> [Google Drive] ~\Documents\Files
 New-Item -Value '~\GoogleDrive\Documents\Files' -Path '~\Documents' -Name 'Files' -ItemType SymbolicLink
 
-# [Win] ~\Works\Create ---> [Google Drive] ~/Works/Create
+# [Win] ~\Works\Create ---> [Google Drive] ~\Works\Create
 New-Item -Value '~\GoogleDrive\Works\Create' -Path '~\Works' -Name 'Create' -ItemType SymbolicLink
 
-# [Win] ~\Works\Others ---> [Google Drive] ~/Works/Others
+# [Win] ~\Works\Others ---> [Google Drive] ~\Works\Others
 New-Item -Value '~\GoogleDrive\Works\Others' -Path '~\Works' -Name 'Others' -ItemType SymbolicLink
 
+
+##### Win 環境から WSL 環境へのリンク #####
+# WSL 側のホームディレクトリパスを変数へ格納
+$WSLHOME = "\\wsl.localhost\Ubuntu\home\${env:USERNAME}"
+
 # [Win] ~\Works\Develop ---> [WSL2] ~/Works/Develop
-$value = "\\wsl.localhost\Ubuntu\home\" + $env:USERNAME + "\Works\Develop"
-New-Item -Value $value -Path '~\Works' -Name 'Develop' -ItemType SymbolicLink
+New-Item -Value "${WSLHOME}\Works\Develop" -Path '~\Works' -Name 'Develop' -ItemType SymbolicLink
 
-# [Win] ~\Videos\Capture ---> [D ドライブ] ~\Videos\Capture
-$value = "D:\" + $env:USERNAME + "\Videos\Capture" # -Value "" 内で変数の展開ができなかったため、初期化してから引数へ指定
-New-Item -Value $value -Path '~\Videos' -Name 'Capture' -ItemType SymbolicLink
-
-# [Win] ~/dotfiles ---> [WSL2] ~/dotfiles
-# 次以降の設定ファイルのリンクに必須
-$value = "\\wsl.localhost\Ubuntu\home\" + $env:USERNAME + "\dotfiles"
-New-Item -Value $value -Path '~' -Name 'dotfiles' -ItemType SymbolicLink
-
-
-##### 設定ファイルのリンク設定 #####
 # PowerShell エイリアス設定ファイル
-New-Item -Value '~\dotfiles\.config\WindowsPowerShell\Microsoft.PowerShell_profile.ps1' -Path '~\Documents\WindowsPowerShell' -Name 'Microsoft.PowerShell_profile.ps1' -ItemType SymbolicLink
+New-Item -Value "${WSLHOME}\dotfiles\.config\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -Path '~\Documents\WindowsPowerShell' -Name 'Microsoft.PowerShell_profile.ps1' -ItemType SymbolicLink
 
 # VSCode 設定ファイル
-New-Item -Value '~\dotfiles\.config\Code\User\settings.json' -Path '~\AppData\Roaming\Code\User' -Name 'settings.json' -ItemType SymbolicLink
-New-Item -Value '~\dotfiles\.config\Code\User\keybindings.json' -Path '~\AppData\Roaming\Code\User' -Name 'keybindings.json' -ItemType SymbolicLink
+New-Item -Value "${WSLHOME}\dotfiles\.config\Code\User\settings.json"    -Path '~\AppData\Roaming\Code\User' -Name 'settings.json'    -ItemType SymbolicLink
+New-Item -Value "${WSLHOME}\dotfiles\.config\Code\User\keybindings.json" -Path '~\AppData\Roaming\Code\User' -Name 'keybindings.json' -ItemType SymbolicLink
 
 # Azure Data Studio 設定ファイル
-New-Item -Value '~\dotfiles\.config\azuredatastudio\User\settings.json' -Path '~\AppData\Roaming\azuredatastudio\User' -Name 'settings.json' -ItemType SymbolicLink
+New-Item -Value "${WSLHOME}\dotfiles\.config\azuredatastudio\User\settings.json" -Path '~\AppData\Roaming\azuredatastudio\User' -Name 'settings.json' -ItemType SymbolicLink
 
-# Git Bash
-New-Item -Value '~\dotfiles\.bashrc' -Path '~' -Name '.bashrc' -ItemType SymbolicLink
+# Git Bash .bashrc
+New-Item -Value "${WSLHOME}\.bashrc" -Path '~' -Name '.bashrc' -ItemType SymbolicLink
 
-# Obsidian
-New-Item -Value '~\dotfiles\.config\Obsidian\MyNotes\.obsidian' -Path '~\Documents\Obsidian\MyNotes' -Name '.obsidian' -ItemType SymbolicLink
+# Obsidian 隠しファイル
+New-Item -Value "${WSLHOME}\dotfiles\.config\Obsidian\MyNotes\.obsidian" -Path '~\Documents\Obsidian\MyNotes' -Name '.obsidian' -ItemType SymbolicLink
+
+
+##### その他 #####
+# [Win] ~\Videos\Capture ---> [D ドライブ] ~\Videos\Capture
+New-Item -Value "D:\${env:USERNAME}\Videos\Capture" -Path '~\Videos' -Name 'Capture' -ItemType SymbolicLink
