9b3f61fa42624efbdf37c510e59db251ba232aa5
kenkenpa198 <kenkenpa198@gmail.com>
2023-04-15 15:31:25 +0900

    [update]split install script for git

--- .setup/Ubuntu/install-apt-packages.sh
+++ .setup/Ubuntu/install-apt-packages.sh
@@ -7,14 +7,8 @@ function main {
     # パッケージリストの取得・既存パッケージの更新
     sudo apt update && sudo apt upgrade -y
 
-    # Git 公式のリポジトリを apt リポジトリへ追加
-    # https://loumo.jp/archives/23149
-    # タイムアウトになる場合、イーサネットアダプターの IPv6 を無効にして実行する
-    sudo add-apt-repository -y ppa:git-core/ppa
-
     # インストールするパッケージの配列を定義
     packages=(
-        "git"
         "gcc"
         "cppcheck"
         "neofetch"
