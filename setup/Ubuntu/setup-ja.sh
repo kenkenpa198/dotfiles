62151f7cc2f75f8792fc30c70f2da01aa7154e19
kenkenpa198 <kenkenpa198@gmail.com>
2023-05-15 18:23:28 +0900

    [update]comment out set locale

--- setup/Ubuntu/setup-ja.sh
+++ setup/Ubuntu/setup-ja.sh
@@ -11,23 +11,10 @@ function main {
     sudo apt-get install -y language-pack-ja
 
     # 適用
-    sudo update-locale LANG=ja_JP.UTF8
+    # sudo update-locale LANG=ja_JP.UTF8
 
     # 日本語版マニュアルのインストール
     sudo apt-get install -y manpages-ja manpages-ja-dev
-
-    # 完了メッセージを表示
-    cat \
-<< msg
-setup-ja.sh の実行を完了しました。
-以下の対応を行ってください。
-
-- ホストを再起動してください。
-- 各ロケール設定が ja_JP.UTF8 へ変更されていることを確認してください。
-    $ locale
-- man コマンドで日本語マニュアルが表示されることを確認してください。
-    $ man apt
-msg
 }
 
 main
