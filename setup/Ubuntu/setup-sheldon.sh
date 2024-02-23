7f83842f738513163f3e0f0f4df10359b056df28
kenkenpa198 <kenkenpa198@gmail.com>
2024-02-23 13:07:26 +0900

    [fix]mod install sheldon script

--- setup/Ubuntu/setup-sheldon.sh
+++ setup/Ubuntu/setup-sheldon.sh
@@ -4,9 +4,6 @@ set -x
 set -euo pipefail
 
 function main {
-    # .giticonfig.local のテンプレートファイルを作成
-    cp ~/dotfiles/.gitconfig.local.example ~/dotfiles/.gitconfig.local
-
     # シンボリックリンクを作成
     ln -sf ~/dotfiles/config/sheldon/plugins.toml ~/.config/sheldon
 
