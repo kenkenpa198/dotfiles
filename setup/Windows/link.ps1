c0391a111a7bab7119fd20d20a21eb458e1c8ef3
kenkenpa198 <kenkenpa198@gmail.com>
2024-03-04 02:54:45 +0900

    [update]change dir name config to app

--- setup/Windows/link.ps1
+++ setup/Windows/link.ps1
@@ -51,15 +51,15 @@ function main {
     $linkHash["${WSLHOME}\works\develop"] = '~\works\develop'
 
     # PowerShell プロファイル
-    $linkHash["${WSLHOME}\dotfiles\config\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"] = '~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1'
+    $linkHash["${WSLHOME}\dotfiles\app\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"] = '~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1'
 
     # VSCode 設定ファイル
-    $linkHash["${WSLHOME}\dotfiles\config\Code\User\snippets"]         = '~\AppData\Roaming\Code\User\snippets'
-    $linkHash["${WSLHOME}\dotfiles\config\Code\User\keybindings.json"] = '~\AppData\Roaming\Code\User\keybindings.json'
-    $linkHash["${WSLHOME}\dotfiles\config\Code\User\settings.json"]    = '~\AppData\Roaming\Code\User\settings.json'
+    $linkHash["${WSLHOME}\dotfiles\app\Code\User\snippets"]         = '~\AppData\Roaming\Code\User\snippets'
+    $linkHash["${WSLHOME}\dotfiles\app\Code\User\keybindings.json"] = '~\AppData\Roaming\Code\User\keybindings.json'
+    $linkHash["${WSLHOME}\dotfiles\app\Code\User\settings.json"]    = '~\AppData\Roaming\Code\User\settings.json'
 
     # Azure Data Studio 設定ファイル
-    $linkHash["${WSLHOME}\dotfiles\config\azuredatastudio\User\settings.json"] = '~\AppData\Roaming\azuredatastudio\User\settings.json'
+    $linkHash["${WSLHOME}\dotfiles\app\azuredatastudio\User\settings.json"] = '~\AppData\Roaming\azuredatastudio\User\settings.json'
 
     # Git Bash .bashrc
     $linkHash["${WSLHOME}\.bashrc"] = '~\.bashrc'
