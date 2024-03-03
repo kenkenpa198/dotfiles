d664118ee9b38b0405a70dc80d2f40b490d42435
kenkenpa198 <kenkenpa198@gmail.com>
2024-03-04 02:44:22 +0900

    [update]Deferred loading with Sheldon

--- setup/Ubuntu/setup-sheldon.sh
+++ setup/Ubuntu/setup-sheldon.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 function main {
     # シンボリックリンクを作成
-    ln -sf ~/dotfiles/config/sheldon/plugins.toml ~/.config/sheldon
+    ln -sf ~/dotfiles/.config/sheldon/plugins.toml ~/.config/sheldon/plugins.toml
 
     set +x
     echo
