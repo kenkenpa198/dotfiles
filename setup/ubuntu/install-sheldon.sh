6a85ee9860241fb5e4b6ecce7711e4bf80faf639
kenkenpa198 <kenkenpa198@gmail.com>
2024-03-08 04:40:14 +0900

    [update]check installed sheldon

--- setup/ubuntu/install-sheldon.sh
+++ setup/ubuntu/install-sheldon.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-# https://sheldon.cli.rs/Installation.html
 
 set -x
 set -euo pipefail
 
 function main {
-    curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh \
-        | bash -s -- --repo rossmacarthur/sheldon --to ~/.local/bin
+    if [ ! -e ~/.local/bin/sheldon ]; then
+        # https://sheldon.cli.rs/Installation.html
+        curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh \
+            | bash -s -- --repo rossmacarthur/sheldon --to ~/.local/bin
+    fi
 
     set +x
     echo
