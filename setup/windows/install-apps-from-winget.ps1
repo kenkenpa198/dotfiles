151d2b4de84fe58ecf8520a2abd508b0ceca7d60
kenkenpa198 <kenkenpa198@gmail.com>
2024-03-09 01:15:49 +0900

    [update]small changed

--- setup/windows/install-apps-from-winget.ps1
+++ setup/windows/install-apps-from-winget.ps1
@@ -1,17 +1,9 @@
-<########################################
-winget で各アプリケーションをインストールする PowerShell スクリプト。
-
-スクリプトの実行には実行ポリシーの変更が必要。
-参考 : https://qiita.com/ponsuke0531/items/4629626a3e84bcd9398f
-
-########################################>
-
 # エラーが発生した時点で処理を中断する
 $ErrorActionPreference = 'Stop'
 
 function main {
     # インストールするアプリケーションのハッシュテーブルを定義
-    $INSTALL_APPS = @(
+    $APPS = @(
         '7zip.7zip'
         'Avidemux.Avidemux'
         'Bitwarden.Bitwarden'
@@ -43,7 +35,7 @@ function main {
         'WinSCP.WinSCP'
     )
 
-    #winget のソースを更新
+    # winget のソースを更新
     winget source update
 
     # winget リポジトリからインストール
@@ -52,7 +44,7 @@ function main {
     # --id                        : インストールをアプリケーションの ID に限定するオプション
     # --accept-source-agreements  : ソースの使用許諾契約に同意するオプション
     # --accept-package-agreements : パッケージの使用許諾契約に同意するオプション
-    foreach ($app in $INSTALL_APPS)
+    foreach ($app in $APPS)
     {
         winget install -s winget -e --id $app --accept-source-agreements --accept-package-agreements
     }
