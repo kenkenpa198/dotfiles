ef70aab5edfc7adea2a77d741f649f787634ceec
kenkenpa198 <kenkenpa198@gmail.com>
2024-03-11 01:25:10 +0900

    [update]small changed

--- setup/linux/header.sh
+++ setup/linux/header.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 # 引数へ渡した文字列の見出しを出力するスクリプト
 
-# Reference:
-# [Bashシェルスクリプトで任意の文字を繰り返し出力する方法 \| ゲンゾウ用ポストイット](https://genzouw.com/entry/2020/10/29/211143/2094/)
-# [ShellScriptにおいて，テキストファイル末尾に改行を加える． \#Bash - Qiita](https://qiita.com/BlackCat_617/items/00ea5db993ed31b56821)
+# 説明と参考文献は次のスクリプトに記載
+#   .local/bin/hr
 
 # example:
 #
-#   $ bash notice.sh script-name
+#   $ bash header.sh "fuga"
 #   ======================================...
-#   Run: script-name
+#   fuga
 #   ======================================...
 
 function hr {
-    # 1. '=' をシェルの幅分の行数で出力
-    # 2. 改行コードを除去して 1 行にする
-    # 3. 改行コードを行末へ加えるために awk を通して出力
     yes '=' | head -n "$(tput cols)" | tr -d '\n' | awk '1'
 }
 
