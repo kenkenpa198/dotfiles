f31feaf0d09387751f54a8dde3a186047908569a
kenkenpa198 <kenkenpa198@gmail.com>
2024-03-16 11:20:23 +0900

    [update]small changed

--- setup/windows/put-wslconfig.ps1
+++
@@ -1,9 +0,0 @@
-# エラーが発生した時点で処理を中断する
-$ErrorActionPreference = 'Stop'
-
-function main {
-    $DOTFILES_HOME = "\\wsl.localhost\Ubuntu\home\${env:USERNAME}\dotfiles"
-    Copy-Item "${DOTFILES_HOME}\.config\WSL\.wslconfig" ~\
-}
-
-main
